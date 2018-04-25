c370687615bc6e9e5c228c1778fce714ec078348
weiyang <179709020@qq.com>
2018-04-25 11:33:41 +0800

    完成1.1.29题，开始1.1.30

--- ex_1_basics/ex_1_BasicProgrammingModel/Ex_29_Equivalent.java
+++ ex_1_basics/ex_1_BasicProgrammingModel/Ex_29_Equivalent.java
@@ -1,14 +1,68 @@
 package ex_1_basics.ex_1_BasicProgrammingModel;
 
+import StdLib.In;
+import StdLib.StdIn;
+
+import java.util.Arrays;
+
 public class Ex_29_Equivalent {
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        int[] whitelist = In.readInts("../txt/largeW.txt");
+
+        System.out.println("加载文件完成， list.length === " + whitelist.length);
+        Arrays.sort(whitelist);
+
+        int[] testArray = {0,0,0,1,1,1,2,2,2,3,4,5,6,7};
+
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            int r = rank(key, testArray);
+            System.out.println("largeW中小于 " +key+ " 的值的数量为 === " + r);
+            int c = count(key, testArray);
+            System.out.println("largeW中等于 " +key+ " 的值的数量为 === " + c);
+
+        }
+
+
+
 
     }
 
+    public static int rank(int key, int[] a) {
+        //同样使用二分法查找key的位置
+        int lo = 0;
+        int hi = a.length-1;
 
+        if (key > a[a.length-1])
+            return a.length;
+        if (key < a[0])
+            return 0;
+
+        while (lo <= hi){
+            int mid = lo + (hi -lo)/2;
+
+            if (key < a[mid]) hi = mid - 1;
+            if (key > a[mid]) lo = mid + 1;
+            if (key == a[mid]) {
+                while (--mid >= 0 && a[mid] == key);
+                return ++mid;
+            }
+        }
+        return 0;
+    }
+
+
+
+    public static int count(int key, int[] a) {
+       int c = 0;
+       for (int i = rank(key, a); i< a.length && a[i] == key; i++) {
+           ++c;
+       }
+
+        return c;
+    }
 
 
 
---
+++ ex_1_basics/ex_1_BasicProgrammingModel/Ex_30_Created2DArray.java
@@ -0,0 +1,4 @@
+package ex_1_basics.ex_1_BasicProgrammingModel;
+
+public class Ex_30_Created2DArray {
+}
