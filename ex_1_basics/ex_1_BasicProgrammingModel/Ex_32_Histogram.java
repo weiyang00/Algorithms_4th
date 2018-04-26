7985ea90c0fbc4849f75e0eb13adeea4a2791863
weiyang <179709020@qq.com>
2018-04-26 09:44:12 +0800

    完成1.1.35 模拟掷骰子

---
+++ ex_1_basics/ex_1_BasicProgrammingModel/Ex_32_Histogram.java
@@ -0,0 +1,43 @@
+package ex_1_basics.ex_1_BasicProgrammingModel;
+
+import StdLib.StdDraw;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex_32_Histogram {
+
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Double> list = new ArrayList<>();
+        while (scanner.hasNextDouble()) {
+            list.add(scanner.nextDouble());
+        }
+        int N = Integer.parseInt(args[0]);
+        double l = Double.parseDouble(args[1]), r = Double.parseDouble(args[2]);
+        int[] num = new int[N];
+        int max = 0;
+        double step = (r - l) / N;
+        for (int i = 0; i < list.size(); i++) {
+            double element = list.get(i);
+            if (element >= l && element <= r) {
+                for (int j = 0; j < N; j++) {
+                    if (element > l + step * j && element <= l + step * j + step) {
+                        num[j]++;
+                        if (max < num[j]) {
+                            max = num[j];
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            double x = (1.0 * i + 0.5) / N;
+            double y = num[i] / (max * 2.0);
+            double rw = 0.4 / N;
+            StdDraw.filledRectangle(x, y, rw, y);
+        }
+    }
+
+}
---
+++ ex_1_basics/ex_1_BasicProgrammingModel/Ex_35_Throw.java
@@ -0,0 +1,113 @@
+package ex_1_basics.ex_1_BasicProgrammingModel;
+
+import StdLib.StdRandom;
+
+public class Ex_35_Throw {
+
+    static int SIDES = 6;
+    static int i = 0;
+
+    public static void main(String[] args) {
+        double[] doubles1 =  throwDiceProbability();
+
+        int j= 0;
+        while (j <= 2*SIDES) {
+            doubles1[j] = getNumberInN(doubles1[j], 3);
+            j++;
+        }
+
+    for (int i = 1 ; ; i++){
+        double[] doubles2 = initial(i);
+        System.out.println("投掷"+i+"次后");
+
+        //数组去小数点之后三位；
+        int k= 0;
+        while (k <= 2*SIDES) {
+            doubles2[k] = getNumberInN(doubles2[k], 3);
+            k++;
+        }
+        if (compareArray(doubles1,doubles2)) {
+            return;
+        }
+    }
+
+    }
+
+    //最终结果是i达到了52741、67514次后，所有点数的概率才能达到小数点后三位的精确度
+
+
+
+
+
+
+    //投色子概率分布
+    public  static double[] throwDiceProbability() {
+
+        double[] dist = new double[2*SIDES + 1];
+        for (int i = 1; i<= SIDES; i++)
+            for (int j = 1; j<=SIDES; j++)
+                dist[i+j] += 1.0;
+
+        for (int k = 2; k<= 2*SIDES; k++)
+            dist[k] /= 36.0;
+
+        for (double d : dist)
+            System.out.println(d);
+
+        return dist;
+    }
+
+
+    //投骰子
+    public static int throwDice() {
+         return StdRandom.uniform(1, SIDES+1) + StdRandom.uniform(1, SIDES+1);
+    }
+
+
+    //开始投掷
+    public static double[] initial(int n) {
+        double[] dices = new double[13];
+
+        for (int i = 0; i < n; i++) {
+            int res = throwDice();
+            dices[res]++;
+        }
+
+        for (int i = 0; i< 13; i++)
+            dices[i] = dices[i] /n;
+
+        return dices;
+    }
+
+    //求绝对值
+    public static double  absolute(double d) {
+        if(d < 0)
+            return -d;
+        return d;
+    }
+
+    //取第n位小数
+    public static int getNumberInN(double d, int n) {
+        int result = 1;
+        for(int i = 0; i < n; i++)
+            result *= 10;
+
+        return (int)(result*d);
+    }
+
+
+    //比较两个数组的值
+    public static boolean compareArray(double[] d1, double[] d2) {
+        if (d1.length != d2.length)
+            return false;
+        for (int i = 0; i < d1.length; i++) {
+            if (d1[i] != d2[i])
+                return false;
+        }
+        return true;
+    }
+
+
+
+
+}
