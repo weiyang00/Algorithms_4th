ac9223fbd901f71861c25f7c7c2c7e48dd5c86a7
weiyang <179709020@qq.com>
2018-05-02 17:17:40 +0800

    开始1.3.19； 部分修改；

---
+++ ex_1_Fundamentals/ex_3_BagQueueStack/Ex_19.java
@@ -0,0 +1,11 @@
+package ex_1_Fundamentals.ex_3_BagQueueStack;
+
+public class Ex_19 {
+
+    Node first= new Node();
+
+
+
+
+
+}
---
+++ ex_1_Fundamentals/ex_3_BagQueueStack/Node.java
@@ -0,0 +1,6 @@
+package ex_1_Fundamentals.ex_3_BagQueueStack;
+
+public class Node<Item> {
+    Item item;
+    Node next;
+}
