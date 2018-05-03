1a48570202983b13e795e9d42543a940c52158cd
weiyang <179709020@qq.com>
2018-05-03 19:09:58 +0800

    部分链表练习题

--- ex_1_Fundamentals/ex_3_BagQueueStack/Ex_19_Linked.java
+++ ex_1_Fundamentals/ex_3_BagQueueStack/Ex_19_Linked.java
@@ -53,6 +53,45 @@ public class Ex_19_Linked {
     }
 
 
+    public  void removeAfter(Node note){
+        for (Node x=first ; x != null; x = x.next)
+            if ( note.equals(x))
+                x.next = null;
+    }
+
+    public  void insertAfter(Node node1, Node node2){
+        for (Node x=first ; x != null; x = x.next)
+            if ( node1.equals(x))
+                x.next = node2;
+    }
+
+    public  void remove(Ex_19_Linked linked, String key){
+        for (Node x=linked.first; x != null; x = x.next)
+            if ( x.next.item.equals(key))
+                x.next= x.next.next;
+    }
+
+    public int max(Node first){
+        if (first.item == null)
+            return 0;
+        int max = (int)first.item;
+        for (Node x = first; x != null; x = x.next)
+            if ((int)x.item > max)
+                max = (int)x.item;
+        return max;
+    }
+
+
+    public int max_recursion(Node first){
+        if (first.item == null)
+            return 0;
+        int max = (int)first.item;
+        int val = max_recursion(first.next);
+        if (val > max)
+            max = val;
+       return max;
+    }
+
 
 
 
