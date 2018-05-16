45689880f963da54fd2dffdd29317024e0ff66bf
weiyang <179709020@qq.com>
2018-05-16 18:52:37 +0800

    添加RandomQueue

---
+++ ex_1_Fundamentals/ex_3_BagQueueStack/Ex_35_RandomQueue.java
@@ -0,0 +1,22 @@
+package ex_1_Fundamentals.ex_3_BagQueueStack;
+
+/**
+ * Created by weiyang on 2018/5/15.
+ *
+ * @author weiyang
+ * @Title: Please fill file name here
+ * @Package
+ * @Description: Please fill description of the file here
+ * @date 2018/5/15
+ */
+public class Ex_35_RandomQueue<Item> {
+
+    Item[] items;
+
+    int N;
+
+
+
+
+
+}
