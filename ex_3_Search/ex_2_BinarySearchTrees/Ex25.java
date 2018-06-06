194c86fb6b7e32c56cfe4d30dbd47e5637f5a4b5
weiyang <179709020@qq.com>
2018-06-06 09:51:51 +0800

    二叉查找树；

---
+++ ex_3_Search/ex_2_BinarySearchTrees/Ex25.java
@@ -0,0 +1,33 @@
+package ex_3_Search.ex_2_BinarySearchTrees;
+
+
+import StdLib.StdIn;
+
+import java.util.Arrays;
+
+public class Ex25 {
+	public static BST<String, Integer> balance(String[] a) {
+		Arrays.sort(a);
+		BST<String, Integer> bst = new BST<>();
+		balance(bst, a, 0, a.length - 1);
+		return bst;
+	}
+
+	private static void balance(BST<String, Integer> bst, String[] a, int lo, int hi) {
+		if (lo > hi) {
+			return;
+		}
+		int mid = lo + (hi - lo) / 2;
+		bst.put(a[mid], mid);
+		balance(bst, a, lo, mid - 1);
+		balance(bst, a, mid + 1, hi);
+	}
+
+	public static void main(String[] args) {
+		String[] a = StdIn.readAllStrings();
+		BST<String, Integer> bst = balance(a);
+		for (String key : bst.keys()) {
+			System.out.println(key + " " + bst.get(key));
+		}
+	}
+}
