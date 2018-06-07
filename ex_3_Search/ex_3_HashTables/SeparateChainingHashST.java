c61544a887e30b6f102163839818b2e6488fdaa1
weiyang <179709020@qq.com>
2018-06-07 19:37:52 +0800

    hashTables；

---
+++ ex_3_Search/ex_3_HashTables/SeparateChainingHashST.java
@@ -0,0 +1,34 @@
+package ex_3_Search.ex_3_HashTables;
+
+
+import ex_3_Search.ex_1_SymbolTable.SequentialSearchST;
+
+public class SeparateChainingHashST<Key, Value> {
+	private int N;
+	private int M;
+	private SequentialSearchST<Key, Value>[] st;
+
+	public SeparateChainingHashST() {
+		this(997);
+	}
+
+	public SeparateChainingHashST(int M) {
+		this.M = M;
+		st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+		for (int i = 0; i < M; i++) {
+			st[i] = new SequentialSearchST();
+		}
+	}
+
+	private int hash(Key key) {
+		return (key.hashCode() & 0x7fffffff) % M;
+	}
+
+	public Value get(Key key) {
+		return (Value) st[hash(key)].get(key);
+	}
+
+	public void put(Key key, Value val) {
+		st[hash(key)].put(key, val);
+	}
+}
