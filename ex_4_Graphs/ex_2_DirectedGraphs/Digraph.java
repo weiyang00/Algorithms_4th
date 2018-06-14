515bdc7bc88740f7583651c9d52a2adb5cb6ebfe
weiyang <179709020@qq.com>
2018-06-14 20:59:56 +0800

    添加有向图

---
+++ ex_4_Graphs/ex_2_DirectedGraphs/Digraph.java
@@ -0,0 +1,99 @@
+package ex_4_Graphs.ex_2_DirectedGraphs;
+
+
+import StdLib.In;
+import ex_1_Fundamentals.ex_3_BagQueueStack.Bag;
+import ex_1_Fundamentals.ex_3_BagQueueStack.Stack;
+
+public class Digraph {
+	private final int V;
+	private int E;
+	private Bag<Integer>[] adj;
+
+	public Digraph(int V) {
+		this.V = V;
+		this.E = 0;
+		adj = (Bag<Integer>[]) new Bag[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new Bag<Integer>();
+		}
+	}
+
+	public Digraph(In in) {
+		this(in.readInt());
+		int E = in.readInt();
+		for (int i = 0; i < E; i++) {
+			int v = in.readInt();
+			int w = in.readInt();
+			addEdge(v, w);
+		}
+	}
+
+	/**
+	 * Exercise 4.2.3
+	 * 
+	 * @param G
+	 */
+	public Digraph(Digraph G) {
+		this(G.V());
+		E = G.E();
+		for (int v = 0; v < G.V(); v++) {
+			Stack<Integer> reverse = new Stack<>();
+			for (int i : G.adj(v)) {
+				reverse.push(i);
+			}
+			for (int i : reverse) {
+				adj[v].add(i);
+			}
+		}
+	}
+
+	public int V() {
+		return V;
+	}
+
+	public int E() {
+		return E;
+	}
+
+	public void addEdge(int v, int w) {
+		adj[v].add(w);
+		E++;
+	}
+
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
+
+	public Digraph reverse() {
+		Digraph R = new Digraph(V);
+		for (int v = 0; v < V; v++) {
+			for (int w : adj(v)) {
+				R.addEdge(w, v);
+			}
+		}
+		return R;
+	}
+
+	// Add for Exercise 4.2.20
+	public int outdegree(int v) {
+		return adj[v].size();
+	}
+
+	public int indegree(int v) {
+		return reverse().adj[v].size();
+	}
+
+	@Override
+	public String toString() {
+		String s = V + " vertices, " + E + " edges\n";
+		for (int v = 0; v < V; v++) {
+			s += v + ": ";
+			for (int w : this.adj(v)) {
+				s += w + " ";
+			}
+			s += "\n";
+		}
+		return s;
+	}
+}
