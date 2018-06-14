4b7a1c41745fadad9e6c7a82acdcea4079b34d46
weiyang <179709020@qq.com>
2018-06-14 17:09:58 +0800

    添加无向图算法；

--- ex_4_Graphs/ex_1_UndirectedGraphs/Graph.java
+++ ex_4_Graphs/ex_1_UndirectedGraphs/Graph.java
@@ -6,26 +6,26 @@ import ex_1_Fundamentals.ex_3_BagQueueStack.Stack;
 
 
 public class Graph {
-	private final int V; // 顶点数目
-	private int E; // 边的数目
-	private Bag<Integer>[] adj; // 邻接表
+	private final int V;					// 顶点数目
+	private int E; 							// 边的数目
+	private Bag<Integer>[] adj; 			// 邻接表
 
 	public Graph(int V) {
 		this.V = V;
 		this.E = 0;
-		adj = (Bag<Integer>[]) new Bag[V];
-		for (int v = 0; v < V; v++) {
+		adj = (Bag<Integer>[]) new Bag[V]; 	// 创建邻接表
+		for (int v = 0; v < V; v++) { 		// 将所有链表初始化为空
 			adj[v] = new Bag<Integer>();
 		}
 	}
 
 	public Graph(In in) {
-		this(in.readInt());
-		int E = in.readInt();
+		this(in.readInt()); // 读取V并将图初始化
+		int E = in.readInt();//读取E
 		for (int i = 0; i < E; i++) {
-			int v = in.readInt();
-			int w = in.readInt();
-			addEdge(v, w);
+			int v = in.readInt(); //读取一个顶点
+			int w = in.readInt(); //读取另一个顶点
+			addEdge(v, w); // 添加一条连接它们的边
 		}
 	}
 
@@ -57,8 +57,8 @@ public class Graph {
 	}
 
 	public void addEdge(int v, int w) {
-		adj[v].add(w);
-		adj[w].add(v);
+		adj[v].add(w); // 将W添加到v的链表中
+		adj[w].add(v); // 将v添加到w的链表中
 		E++;
 	}
 
---
+++ ex_4_Graphs/ex_1_UndirectedGraphs/TestSearch.java
@@ -0,0 +1,25 @@
+package ex_4_Graphs.ex_1_UndirectedGraphs;
+
+
+import StdLib.In;
+import StdLib.StdOut;
+
+public class TestSearch {
+	public static void main(String[] args) {
+		Graph G = new Graph(new In(args[0]));
+		int s = Integer.parseInt(args[1]);
+		DepthFirstSearch search = new DepthFirstSearch(G, s);
+
+		for (int v = 0; v < G.V(); v++) {
+			if (search.marked(v)) {
+				StdOut.print(v + " ");
+			}
+		}
+		StdOut.println();
+
+		if (search.count() != G.V()) {
+			StdOut.print("NOT ");
+		}
+		StdOut.println("connected");
+	}
+}
